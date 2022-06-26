e1df23e0e6284d8216b7f4a910eacb47938dfe15
Александр Зарубин <zarubin_14@list.ru>
2022-06-26 16:21:30 +0300

    End of work

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 import express from 'express'
 import path from 'path'
 import 'dotenv/config'
+
 const PORT = process.env.PORT || 3000
 const app = express()
 const __dirname = path.resolve(path.dirname(''));
 
-app.use(express.static(path.resolve(__dirname, 'public' )))
+// const cards = document.querySelectorAll(".card")
+// const index = document.getElementById(`${id}`)
+
+app.use(express.static(path.resolve(__dirname, 'public')))
 
 
-app.get('/', (req,res)=>{
-res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
-app.get('/product', (req,res)=>{
-res.sendFile(path.resolve(__dirname, 'public', 'product.html'))
+app.get('/product1', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'product.html'))
+})
+app.get('/product2', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'product.html'))
 })
 
 app.listen(PORT, () => {
-console.log(`Server started at PORT ${PORT}`)
+  console.log(`Server started at PORT ${PORT}`)
 })
